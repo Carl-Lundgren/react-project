3ea1bdf9b14888cae64ce1aa2e6339865e0e44f5
Carl <lundgren.e.carl@gmail.com>
2021-06-24 18:41:52 -0700

    Dog component buttons

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react'
 
 export default class Dog extends Component {
     state = {
-        liked: false,
-        favorited: false
+        like: false,
+        favorite: false
+    }
+
+    function button(e, type){
+        e.preventDefault
+        this.setState({
+            type: !this.state[type]
+        })
+        if (e.style.backgroundColor === '000') {
+            e.style.backgroundColor = 'F00';
+        } else {
+            e.style.backgroundColor = '000';
+        }
     }
 
     render() {
         return (
             <div>
                 <img src={pic} alt="Cute Dog" width="250" />
-
+                <button onSubmit={button(e, like)} style={{color:FFF}, {backgroundColor:000}}> Like</button>
+                <button onSubmit={button(e, favorite)} style={{color:FFF}, {backgroundColor:000}}> Favorite</button>
             </div>
         )
     }
